29f1b444e00ed66565765fa39c0dd08173e63c41
42290931@qq.com <42290931@qq.com>
2017-07-24 18:44:11 +0800

    t

--- backend/views/category/_tab_menu.php
+++ backend/views/category/_tab_menu.php
@@ -4,8 +4,20 @@ use yii\bootstrap\Nav;
 echo Nav::widget([
     'items' => [
         [
-            'label' => '商品分类',
+            'label' => '【产品】分类',
             'url' => ['category/index'],
+        ],
+        [
+            'label' => '【关于我们】分类',
+            'url' => ['category/aboult'],
+        ],
+        [
+            'label' => '【技术支持】分类',
+            'url' => ['category/support'],
+        ],
+        [
+            'label' => '【新闻中心】分类',
+            'url' => ['category/news'],
         ],
         // [
         //     'label' => '新增导航',
